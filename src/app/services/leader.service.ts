0f026c677c58bfb6125ad8d9b2cd2e52db003b57
Steve Sinai <ssinai@yahoo.com>
2018-07-21 14:31:31 -0700

    Promise Part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -11,14 +11,32 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
+    });
   }
+
+  // getLeaders(): Promise<Leader[]> {
+  //   return Promise.resolve(LEADERS);
+  // }
+  //
+  // getLeader(id: number): Promise<Leader> {
+  //   return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+  // }
+  //
+  // getFeaturedLeader(): Promise<Leader> {
+  //   return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+  // }
 }
