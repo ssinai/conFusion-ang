740556db2f4b61c6b9349bb0aba12ca8ca08098d
Steve Sinai <ssinai@yahoo.com>
2018-08-11 03:46:04 -0700

    Week 3 assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -65,6 +66,7 @@ import { LoginComponent } from './login/login.component';
     MatSlideToggleModule,
     ReactiveFormsModule,
     MatProgressSpinnerModule,
+    MatSliderModule
   ],
   providers: [
     DishService,
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -13,24 +15,94 @@ import { switchMap } from 'rxjs/operators';
 
 export class DishdetailComponent implements OnInit {
 
+  @ViewChild('cform') commentFormDirective;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be at least 2 characters long.'
+    },
+    'comment': {
+      'required': 'Comment is required.'
+    }
+  };
+
   dish: Dish;
   dishIds: number[];
   prev: number;
   next: number;
 
+  commentForm: FormGroup;
+  comment: Comment;
+
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) {
+      // this.createForm()
+    }
 
     ngOnInit() {
+      this.createForm()
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
       this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(+params['id'])))
         .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
     }
 
-  goBack(): void {
-    this.location.back();
-  }
+    createForm() {
+      this.commentForm = this.fb.group({
+        author: ['', [Validators.required, Validators.minLength(2)]],
+        rating: '5',
+        comment: ['', [Validators.required]]
+      });
+
+      this.commentForm.valueChanges
+        .subscribe(data => this.onValueChanged(data));
+
+      this.onValueChanged(); // (re)set validation messages now
+    }
+
+    onSubmit() {
+      this.comment = this.commentForm.value;
+      this.comment.date = new Date().toISOString();
+      console.log(this.comment);
+      this.dish.comments.push(this.comment)
+      this.commentFormDirective.resetForm();
+      this.commentForm.reset({
+        author: '',
+        rating: '5',
+        comment: ''
+      });
+    }
+
+    onValueChanged(data?: any) {
+      if (!this.commentForm) { return; }
+      const form = this.commentForm;
+      for (const field in this.formErrors) {
+        if (this.formErrors.hasOwnProperty(field)) {
+          // clear previous error message (if any)
+          this.formErrors[field] = '';
+          const control = form.get(field);
+           if (control && control.dirty && !control.valid) {
+            const messages = this.validationMessages[field];
+            for (const key in control.errors) {
+              if (control.errors.hasOwnProperty(key)) {
+                this.formErrors[field] += messages[key] + ' ';
+              }
+            }
+          }
+        }
+      }
+    }
+
+    goBack(): void {
+      this.location.back();
+    }
 
   setPrevNext(dishId: number) {
     const index = this.dishIds.indexOf(dishId);
